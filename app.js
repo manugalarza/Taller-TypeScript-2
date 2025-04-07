1a434b4c4fc6be44130731d50ad4c8d83828a24a
manugalarza <157720138+manugalarza@users.noreply.github.com>
2025-04-06 23:09:05 -0500

    Copiar taller 1

---
+++ app.js
@@ -0,0 +1,17 @@
+import { series } from "./data.js";
+const tableBody = document.querySelector("#series-table tbody");
+const averageRow = document.getElementById("average-row");
+let totalSeasons = 0;
+series.forEach((serie) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>${serie.id}</td>
+    <td>${serie.name}</td>
+    <td>${serie.channel}</td>
+    <td>${serie.seasons}</td>
+  `;
+    tableBody.appendChild(row);
+    totalSeasons += serie.seasons;
+});
+const average = totalSeasons / series.length;
+averageRow.innerHTML = `<strong>Seasons average: ${average.toFixed(2)}</strong>`;
